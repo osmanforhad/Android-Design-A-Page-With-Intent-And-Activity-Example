6abc1bf28d31cdc3e16f6bf1a54b4a5244154314
osmanforhad <osmanforhad93@gmail.com>
2018-12-18 23:50:31 +0600

    How to Design a Page in Android XML And Exampole of Intent and Activity

---
+++ Android-Design-A-Page-With-Intent-And-Activity-Example/FirstApplication/app/src/main/java/com/osmanforhad/firstapplication/MainActivity.java
@@ -0,0 +1,32 @@
+package com.osmanforhad.firstapplication;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button b;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        b = (Button) findViewById(R.id.mybutton);
+
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Clicked on Button & we are going to second activity..", Toast.LENGTH_LONG).show();
+
+                Intent i = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(i);
+
+            }
+        });
+    }
+}
